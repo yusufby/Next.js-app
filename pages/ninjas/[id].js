dc13f697fd726b7948669f8fb4c46317f9ff8b4a
yusufby <yusufbayram19891989@gmail.com>
2023-07-19 11:00:39 +0100

    sdsa

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,7 +1,32 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+   const data = await res.json();
+
+   const paths=data.map((ninja)=>{
+      return {
+         param:{id:ninja.id.toString()}
+      }
+   })
+   return {
+      paths, 
+      fallback:false
+   }
+}
+
+export const getStaticProps=async()=>{
+   const id=context.params.id;
+   const res= await fetch('https://jsonplaceholder.typicode.com/users/'+ id);
+   const data=await res.json();
+
+
+}
+const Details = (ninja) => {
    return (<div>
 
-      <h1>Details</h1>
+      <h1>{ninja.id}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
    </div>);
 }
 
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,23 +1,25 @@
 import styles from "../../styles/ninjas.module.css"
 import Link from 'next/link'
 
+
+
+
 export const getStaticProps=async()=>{
 const res=await fetch('https://jsonplaceholder.typicode.com/users');
 const data=await res.json();
-czczxczc
 return {
    props:{ninjas:data}
 }
 }
-
 const Ninjas = ({ninjas}) => {
-   
+
    return (<div>
       <h1>Ninjas</h1>
       {ninjas.map(ninja=>(
-         <Link legacyBehavior href={'./ninjas/' + ninja.id} key={ninja.id}>  
+         <Link legacyBehavior href={'./ninjas/' + ninja.id} key={ninja.id} >  
          <a className={styles.single}>
          <h3>{ninja.name}</h3>
+         <h3>{ninja.email}</h3>
           </a>
          </Link>
       ))}
