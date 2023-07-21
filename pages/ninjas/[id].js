c150a8b63691e6aac4bcb9555c3d3570d4e3615b
yusufby <yusufbayram19891989@gmail.com>
2023-07-21 20:26:40 +0100

    id

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,32 +2,36 @@ export const getStaticPaths = async () => {
    const res = await fetch('https://jsonplaceholder.typicode.com/users');
    const data = await res.json();
 
-   const paths=data.map((ninja)=>{
+   const paths = data.map((ninja) => {
       return {
-         param:{id:ninja.id.toString()}
+         param: { id: ninja.id.toString() }
       }
    })
    return {
-      paths, 
-      fallback:false
+      paths,
+      fallback: false
    }
 }
 
-export const getStaticProps=async()=>{
-   const id=context.params.id;
-   const res= await fetch('https://jsonplaceholder.typicode.com/users/'+ id);
-   const data=await res.json();
+export const getStaticProps = async (context) => {
+   const id = context.params.id;
+   const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+   const data = await res.json();
+
+   return {
+      props: { ninja: data }
+   }
 
 
 }
-const Details = (ninja) => {
+const Details = ({ninja}) => {
    return (<div>
 
       <h1>{ninja.id}</h1>
       <p>{ninja.email}</p>
       <p>{ninja.website}</p>
       <p>{ninja.address.city}</p>
-   </div>);
+   </div>); ``
 }
 
 export default Details;
